6abce46d5fdd265b3a62a2118b63fc967281d676
Dinisynch <dinisynch0@gmail.com>
2025-05-02 17:08:00 +0300

    feat: added JWT token generation and validation

--- app/database.py
+++ app/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
-from app.core.config import settings
+from app.core.config import get_db_url
 
-engine = create_async_engine(settings.DATABASE_URL, echo=True)
+engine = create_async_engine(get_db_url(), echo=True)
 
 new_async_session = async_sessionmaker(engine, expire_on_commit=False)
 
