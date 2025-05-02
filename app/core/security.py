e0abd337da89606f32b778f03dcb69ed6a969dce
Dinisynch <dinisynch0@gmail.com>
2025-05-02 18:58:53 +0300

    feat: created endpoints for user registration, login

--- app/core/security.py
+++ app/core/security.py
@@ -1,4 +1,7 @@
 from passlib.context import CryptContext
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.models.user import User
 
 
 pwd_context = CryptContext(schemes=["bcrypt"],
@@ -9,3 +12,12 @@ def get_password_hash(password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+async def authenticate_user(email: str, password: str, session: AsyncSession) -> User | None:
+    stmt = select(User).where(User.email == email)
+    result = await session.execute(stmt)
+    user = result.scalar_one_or_none()
+
+    if not user or not verify_password(plain_password=password, hashed_password=user.password):
+        return None
+    return user
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -4,6 +4,8 @@ from pydantic import BaseModel, EmailStr, Field
 class SUserRegister(BaseModel):
     email: EmailStr
     password: str = Field(..., min_length=5, max_length=50)
+    first_name: str = Field(..., min_length=1, max_length=50, description="Имя, от 1 до 50 символов")
+    last_name: str = Field(..., min_length=1, max_length=50, description="Фамилия, от 1 до 50 символов")
 
 
 class SUserAuth(BaseModel):
